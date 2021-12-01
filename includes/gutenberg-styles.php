dac1299c683270d37aa71623dd902578b0afde85
Luigi <luigicruz.dev@gmail.com>
2021-12-01 08:32:39 +0800

    wip

--- includes/gutenberg-styles.php
+++ includes/gutenberg-styles.php
@@ -12,8 +12,8 @@ function kl_block_editor_styles() {
     $version
 	);
 
-	$version = filemtime(get_stylesheet_directory() . '/src/styles/_all.css');
-	wp_enqueue_style('regular-styles', get_stylesheet_directory_uri() . '/src/styles/_all.css', [], $version);
+	$version = filemtime(get_stylesheet_directory() . '/src/styles/styles.css');
+	wp_enqueue_style('regular-styles', get_stylesheet_directory_uri() . '/src/styles/styles.css', [], $version);
   //wp_enqueue_script('feather-icons', 'https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js');
 }
 add_action('enqueue_block_editor_assets', 'kl_block_editor_styles');
--- partials/footer/footer.php
+++ partials/footer/footer.php
@@ -1,31 +1,34 @@
 <footer class="footer">
   <div class="footer__container container">
-    <div class="footer__logo-row">
-      <a class="footer__logo" href="/">
+    <div class="footer__logo-row grid">
+      <a class="footer__logo grid__col--span5" href="/">
         <img class="footer__logo-img" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/logo.svg" alt="<?php echo get_bloginfo('name') ?>">
       </a>
     </div>
     <div class="footer__navs grid">
-      <nav class="footer__primary-nav grid__col--span3">
+      <nav class="footer__primary-nav grid__col--span6">
         <?php render_menu('primary', 'footer__primary-nav', '-') ?>
       </nav>
     </div>
     <div class="footer__text grid">
-      <div class="footer__copy grid__col--span3">
+      <!-- <div class="footer__copy grid__col--span3">
         &copy; <?php echo get_bloginfo('name') ?>. All rights reserved. 
       </div>
       <nav class="footer__menu grid__col--span7">
         <?php render_menu('footer', 'footer__menu', '-') ?>
-      </nav>
-      <div class="footer__social grid__col--span2">
+      </nav> -->
+      <div class="footer__social grid__col--span12">
         <a class="footer__social-link" href="#">
-          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/facebook-icon.svg">
+          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/facebook-icon.svg?v=0.1.0">
         </a>
         <a class="footer__social-link" href="#">
-          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/twitter-icon.svg">
+          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/twitter-icon.svg?v=0.1.0">
         </a>
         <a class="footer__social-link" href="#">
-          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/linkedin-icon.svg">
+          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/linkedIn-icon.svg?v=0.1.0">
+        </a>
+        <a class="footer__social-link" href="#">
+          <img class="footer__social-icon" src="<?php echo get_stylesheet_directory_uri() ?>/src/images/instagram-icon.svg?v=0.1.0">
         </a>
       </div>
     <div>
